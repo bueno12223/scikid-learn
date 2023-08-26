431e5fb603a6c97153f69f804a201bcee4c21f22
Jesus Berrio <berriojesus122@gmail.com>
2023-08-26 09:28:48 -0500

    Base of the model

---
+++ main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import sklearn
+import matplotlib.pyplot as plt
+
+from sklearn.decomposition import PCA
+from sklearn.decomposition import IncrementalPCA
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+
+function main():
+    
+
+if __name__ == "__main__":
